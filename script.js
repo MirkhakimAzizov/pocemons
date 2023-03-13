cfc76133f57c8d4cc26d044089083cc665bcbb8d
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-03-13 13:31:18 +0500

    sorted one

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-"use strice";
+"use strick";
+
 
 let elList = document.querySelector(".pacemons__list");
+let elForm = document.querySelector(".hero__form");
+let elSelect = document.querySelector(".hero__select");
+let elSearch = document.querySelector(".hero__search");
 
-render(pokemons);
+renderUi(pokemons);
 
-function render(arr) {
+function renderUi(arr) {
 
     for (let r of arr) {
 
@@ -17,10 +21,10 @@ function render(arr) {
             <img src="${r.img}" alt="${r.name}">
         </div>
         <div class="pacemons__info">
-                <h3 class="pacemons__title">${r.name}</h3>
-                <p class="pacemons__text">${r.type}</p>
-                <span class="pacemons__weight">${r.weight}</span>
-                <span class="pacemons__age">99 age</span>
+            <h3 class="pacemons__title">${r.name}</h3>
+            <p class="pacemons__text">${r.type}</p>
+            <span class="pacemons__weight">${r.weight}</span>
+            <span class="pacemons__age">Age: ${r.avg_spawns}</span>
         </div>
     `
 
@@ -28,3 +32,57 @@ function render(arr) {
 
     }
 };
+
+
+let candy = [];
+
+function candies() {
+    pokemons.forEach(items => {
+        if (!candy.includes(items.candy)) {
+            candy.push(items.candy);
+        }
+    });
+    candy.forEach((item) => {
+        let elOption = document.createElement('option');
+        elOption.textContent = item;
+        elSelect.append(elOption);
+    });
+}
+candies();
+
+elForm.addEventListener('change', (evt) => {
+    evt.preventDefault();
+    let value = elSelect.value;
+
+    elList.innerHTML = '';
+
+    if (value == 'all') {
+        renderUi(pokemons)
+    } else {
+        const candyArr = pokemons.filter((item) => {
+            return value === item.candy;
+        });
+        renderUi(candyArr);
+
+    }
+});
+
+elSearch.addEventListener("keyup", (evt) => {
+    evt.preventDefault();
+    let value;
+
+    if (elSearch.value != " ") {
+        value = evt.target.value;
+
+        let nameArr = pokemons.filter(item => {
+            return item.name.includes(value);
+        })
+
+        elList.innerHTML = '';
+
+        renderUi(nameArr);
+    } else {
+        renderUi(pokemons);
+    }
+
+});
